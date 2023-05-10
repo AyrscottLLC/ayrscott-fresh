4b0c87b5408c8b05df7a0d4981da36c9172eace3
Jared De Blander <jared0x90@gmail.com>
2023-05-09 23:22:02 -0400

    ayrscott-fresh website initial commit

---
+++ routes/index.tsx
@@ -0,0 +1,18 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>
+          Ayrscott, LLC - Web, Automation & Software Engineering Services
+          located in Mechanicsville, VA (RVA)
+        </title>
+      </Head>
+      <div>
+        <h1>Ayrscott, LLC</h1>
+        <h2>Copyright &copy; 2023</h2>
+      </div>
+    </>
+  );
+}
