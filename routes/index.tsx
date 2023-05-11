81bca749cfd844afbe6e5dc2d8951c1b84f46b7a
Jared De Blander <jared0x90@gmail.com>
2023-05-11 05:37:18 -0400

    reset layout to twind default

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,123 +1,23 @@
 import { Head } from "$fresh/runtime.ts";
+import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <div>
-          At Ayrscott we bring your visions to life in the digital world. By
-          transforming your ideas into powerful software we can simplify your
-          complex processes through automation. We are a small team of
-          experienced software engineers that can help you with your next
-          project.
-        </div>
-        <div>
-          We specialize in a range of modern web platforms and provide custom
-          software development and API integrations through a host of modern
-
-          languages including PHP, Python, C#, JavaScript, SQL and more.
-
-          Further, we provide a range of development services for sensing and
-          electronics.
-        </div>
-
-        <div>
-          If you'd like to learn more about our founder Jared De Blander, head
-          on over to their GitHub profile.
-        </div>
-
-        <div>
-          Curious if we could help in your next project? Contact us today and
-          let's talk!
-        </div>
-        <div>
-          <p>
-            We provide a range of web, software development and engineering
-            services including&hellip;
-          </p>
-          <ul>
-            <li>
-              Web Solutions
-              <ul>
-                <li>Cloud &amp; Virtual machine management</li>
-                <li>Third-party API integrations</li>
-                <li>Bespoke PHP, Python &amp; JavaScript solutions</li>
-              </ul>
-            </li>
-            <li>
-              Database Design, Clustering &amp; Administration
-              <ul>
-                <li>
-                  Relational (SQL)
-                  <ul>
-                    <li>MySQL</li>
-                    <li>MariaDB</li>
-                    <li>PostgreSQL</li>
-                    <li>SQLite</li>
-                  </ul>
-                </li>
-                <li>
-                  Document (NoSQL)
-                  <ul>
-                    <li>MongoDB</li>
-                    <li>CouchDB</li>
-                    <li>Memcache</li>
-                    <li>Redis</li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li>
-              Electronics &amp; Prototyping
-              <ul>
-                <li>
-                  Frameworks
-                  <ul>
-                    <li>
-                      <a href="https://www.arduino.cc/">Arduino</a> /{" "}
-                      <a href="https://www.adafruit.com/">Adafruit</a>
-                    </li>
-                    <li>
-                      <a href="https://platformio.org/">PlatformIO</a>
-                    </li>
-                    <li>
-                      <a href="https://www.raspberrypi.org/">RaspberryPi</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  Wireless Sensing &amp; Telemetry
-                  <ul>
-                    <li>WiFi</li>
-                    <li>Mobile / LTE</li>
-                    <li>
-                      <a href="https://lora-alliance.org/">LoRa Alliance</a>
-                      {" "}
-                      (Long Range Wireless)
-                    </li>
-                    <li>
-                      <a href="https://www.airspayce.com/mikem/arduino/RadioHead/">
-                        RadioHead
-                      </a>{" "}
-                      Packet Radio
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li>
-              IT Consulting
-              <ul>
-                <li>Voice over IP (VoIP / SIP)</li>
-                <li>Virtual Private Networking (VPN)</li>
-                <li>Software Defined Networks (SDN)</li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-        <div>
-          Copyright &copy; 2023
-        </div>
+      <Head>
+        <title>Ayrscott, LLC</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <img
+          src="/logo.svg"
+          class="w-32 h-32"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p class="my-6">
+          Welcome to `fresh`. Try updating this message in the
+          ./routes/index.tsx file, and refresh.
+        </p>
+        <Counter start={3} />
       </div>
     </>
   );
--- twind.config.ts
+++ twind.config.ts
@@ -2,13 +2,4 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
-  theme: {
-    extend: {
-      colors: {
-        foreground: "#eee",
-        background: "#222",
-        nav_red: "#333",
-      },
-    },
-  },
 } as Options;
